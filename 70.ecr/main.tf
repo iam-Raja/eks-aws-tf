d3ed3662d9756c907ea6881020b779ded90b8415
iam-raja <rajapeta45@gmail.com>
2024-07-17 15:17:02 +0530

    eks

--- 70.ecr/main.tf
+++ 70.ecr/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecr_repository" "frontend" {
 }
 
 resource "aws_ecr_repository" "backend" {
-  name                 = "${var.project_name}--backend"
+  name                 = "${var.project_name}-backend"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
